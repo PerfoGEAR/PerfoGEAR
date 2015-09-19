f19dfd163ab4753519cef51db47924fc9dbe3bfc
Mohammad Montazeri <Mo121ntazeri@gmail.com>
2015-09-19 19:39:14 +0430

    Test Route

--- public/index.php
+++ public/index.php
@@ -1,2 +1,27 @@
 <?php
-    echo 'Salam';
+
+//require __DIR__.'/../vendor/PerfoGEAR/kernel.php';
+//
+//$app = new kernel();
+
+$start = microtime(true);
+class route{
+
+    public static $get = array();
+
+    public static function get($url, $func)
+    {
+        route::$get[] = [$url, $func];
+    }
+}
+
+route::get('s', ['s' => function(){
+    echo 's2';
+}]);
+
+call_user_func(route::$get[0][1]['s']);
+
+
+$end = microtime(true) - $start;
+
+echo $end;
